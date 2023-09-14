1d8f05539fb60611d698a7e727ee4315dbb1d4dd
EdwradJhosep <1454668@senati.pe>
2023-09-14 11:33:20 -0500

    creacion del login smple mas la creacion del archivo de regsiter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MI_WEB.Controllers
         {
             return View();
         }
+        public IActionResult register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
